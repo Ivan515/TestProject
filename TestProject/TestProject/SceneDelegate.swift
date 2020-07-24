c80e10e2da997f8649fa3f292b15171fa4e494f2
Ivan Apet <Apet.ivan1@gmail.com>
2020-07-24 13:06:54 +0300

    [*] Improvement to set first VC

--- TestProject/TestProject/SceneDelegate.swift
+++ TestProject/TestProject/SceneDelegate.swift
@@ -18,6 +18,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let storyboard = UIStoryboard.main
+        let vc = storyboard.instantiate(GeneralViewController.self)
+        let navig = UINavigationController(rootViewController: vc)
+        if let windowScene = scene as? UIWindowScene {
+            window = UIWindow(windowScene: windowScene)
+            window?.rootViewController = navig
+            window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
